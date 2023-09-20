70ca21151ff269ed98cba920f3e455aa1d5a41ca
POLYBALAS <136475005+POLYBALAS@users.noreply.github.com>
2023-09-19 21:14:36 -0300

    Add files via upload

---
+++ testea-main/mostrarForm.js
@@ -0,0 +1,24 @@
+var formularioAtual = null;
+
+        function mostrarFormulario(id) {
+            if (formularioAtual !== null) {
+                document.getElementById(formularioAtual).style.display = 'none';
+            }
+            document.getElementById(id + 'Form').style.display = 'block';
+            formularioAtual = id + 'Form';
+        }
+
+        document.getElementById('btnInicio').addEventListener('click', function() {
+            mostrarFormulario('inicio');
+        });
+        document.getElementById('btnRoteirizacao').addEventListener('click', function() {
+            mostrarFormulario('roteirizacao');
+        });
+        document.getElementById('btnAgendaGestores').addEventListener('click', function() {
+            mostrarFormulario('agendaGestores');
+        });
+        document.getElementById('btnMapaClientes').addEventListener('click', function() {
+            mostrarFormulario('mapaClientes');
+        });
+
+        mostrarFormulario('inicio');
